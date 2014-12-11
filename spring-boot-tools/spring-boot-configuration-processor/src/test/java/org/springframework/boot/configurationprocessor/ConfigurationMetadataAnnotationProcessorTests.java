87d526634d0c2a7eddf90c355f6f08010e320d1c
Stephane Nicoll <snicoll@pivotal.io>
2014-12-11 17:51:26 +0100

    Support of Lombok annotated ConfigurationProperties
    Previously, no configuration properties were discovered on a class using
    lombok instead of regular getters/setters.
    
    This commit adds a support for some of the lombok annotations,
    specifically that is @Data, @Getter and @Setter. Provides the same
    semantic as what lombok is generating.
    
    Closes gh-2114

--- spring-boot-tools/spring-boot-configuration-processor/src/test/java/org/springframework/boot/configurationprocessor/ConfigurationMetadataAnnotationProcessorTests.java
+++ spring-boot-tools/spring-boot-configuration-processor/src/test/java/org/springframework/boot/configurationprocessor/ConfigurationMetadataAnnotationProcessorTests.java
@@ -17,7 +17,6 @@
 package org.springframework.boot.configurationprocessor;
 
 import java.io.IOException;
-
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
@@ -25,7 +24,10 @@ import javax.lang.model.SourceVersion;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+
 import org.springframework.boot.configurationprocessor.metadata.ConfigurationMetadata;
+import org.springframework.boot.configurationsample.lombok.LombokSimpleDataProperties;
+import org.springframework.boot.configurationsample.lombok.LombokSimpleProperties;
 import org.springframework.boot.configurationsample.method.EmptyTypeMethodConfig;
 import org.springframework.boot.configurationsample.method.InvalidMethodConfig;
 import org.springframework.boot.configurationsample.method.MethodAndClassConfig;
@@ -286,13 +288,43 @@ public class ConfigurationMetadataAnnotationProcessorTests {
 		assertThat(metadata, not(containsProperty("excluded.writer-array")));
 	}
 
+	@Test
+	public void lomboxDataProperties() throws Exception {
+		ConfigurationMetadata metadata = compile(LombokSimpleDataProperties.class);
+		assertSimpleLombokProperties(metadata, LombokSimpleDataProperties.class, "ldata");
+	}
+
+	@Test
+	public void lomboxSimpleProperties() throws Exception {
+		ConfigurationMetadata metadata = compile(LombokSimpleProperties.class);
+		assertSimpleLombokProperties(metadata, LombokSimpleProperties.class, "lsimple");
+	}
+
+	private void assertSimpleLombokProperties(ConfigurationMetadata metadata, Class<?> source, String prefix) {
+		assertThat(metadata, containsGroup(prefix).fromSource(source));
+		assertThat(metadata, not(containsProperty(prefix + ".id")));
+		assertThat(
+				metadata,
+				containsProperty(prefix + ".name", String.class)
+						.fromSource(source)
+						.withDescription("Name description."));
+		assertThat(metadata, containsProperty(prefix + ".description"));
+		assertThat(metadata, containsProperty(prefix + ".counter"));
+		assertThat(metadata,
+				containsProperty(prefix + ".number").fromSource(source)
+						.withDefaultValue(is(0))
+						.withDeprecated());
+		assertThat(metadata, containsProperty(prefix + ".items"));
+		assertThat(metadata, not(containsProperty(prefix + ".ignored")));
+	}
+
 	private ConfigurationMetadata compile(Class<?>... types) throws IOException {
 		TestConfigurationMetadataAnnotationProcessor processor = new TestConfigurationMetadataAnnotationProcessor();
 		new TestCompiler(this.temporaryFolder).getTask(types).call(processor);
 		return processor.getMetadata();
 	}
 
-	@SupportedAnnotationTypes({ "*" })
+	@SupportedAnnotationTypes({"*"})
 	@SupportedSourceVersion(SourceVersion.RELEASE_6)
 	private static class TestConfigurationMetadataAnnotationProcessor extends
 			ConfigurationMetadataAnnotationProcessor {
