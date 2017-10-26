3c3ca105127a68c6891f9c2f83a95a7b7e92feed
Stephane Nicoll <snicoll@pivotal.io>
2017-10-26 15:27:12 +0200

    Polish "Add runApplication() Kotlin top level function"
    Closes gh-10511

--- spring-boot-project/spring-boot/src/test/kotlin/org/springframework/boot/SpringApplicationExtensionsTests.kt
+++ spring-boot-project/spring-boot/src/test/kotlin/org/springframework/boot/SpringApplicationExtensionsTests.kt
@@ -18,7 +18,7 @@ package org.springframework.boot
 import org.junit.Assert.*
 import org.junit.Test
 import org.springframework.beans.factory.getBean
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.MockServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.StandardEnvironment
@@ -110,8 +110,8 @@ class SpringApplicationExtensionsTests {
 	internal open class ExampleWebConfig {
 
 		@Bean
-		open fun webServer(): TomcatServletWebServerFactory {
-			return TomcatServletWebServerFactory(0)
+		open fun webServer(): MockServletWebServerFactory {
+			return MockServletWebServerFactory()
 		}
 	}
 
