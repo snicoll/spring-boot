abca29d48c8def11598f9def15244e97e4ce31bc
Stephane Nicoll <snicoll@pivotal.io>
2018-06-06 14:44:26 +0200

    Add support for connection pooling with Artemis
    This commit expands ActiveMQ's connection pooling to artemis. The same
    pooling features are now shared by the two brokers and a
    PooledConnectionFactory can be auto-configured when the necessary jar is
    present.
    
    See gh-12161

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jms/artemis/ArtemisProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jms/artemis/ArtemisProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.activemq.artemis.core.remoting.impl.invm.TransportConstants;
 
+import org.springframework.boot.autoconfigure.jms.activemq.PooledConnectionFactoryProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -61,6 +62,8 @@ public class ArtemisProperties {
 	private String password;
 
 	private final Embedded embedded = new Embedded();
+
+	private final PooledConnectionFactoryProperties pool = new PooledConnectionFactoryProperties();
 
 	public ArtemisMode getMode() {
 		return this.mode;
@@ -104,6 +107,10 @@ public class ArtemisProperties {
 
 	public Embedded getEmbedded() {
 		return this.embedded;
+	}
+
+	public PooledConnectionFactoryProperties getPool() {
+		return this.pool;
 	}
 
 	/**
