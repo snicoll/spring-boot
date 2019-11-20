6c095d3746974065b0997cd163e16c0b1c8336d4
Andy Wilkinson <awilkinson@pivotal.io>
2019-11-20 12:04:45 +0000

    Provide an overview and details of a goal's parameters

--- spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/groovy/generateGoalsDocumentation.groovy
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/groovy/generateGoalsDocumentation.groovy
@@ -14,14 +14,43 @@ private String format(String input) {
 		.replaceAll('<a href=."(.*?)".>(.*?)</a>', '\$1[\$2]')
 }
 
-private writeParameters(PrintWriter writer, def parameters, def configuration, def sectionId) {
+private writeParametersTable(PrintWriter writer, def goal, def parameters, def configuration) {
+	writer.println '[cols="3,2,3"]'
+	writer.println '|==='
+	writer.println '| Name | Type | Default'
+	writer.println()
+	parameters.each { parameter ->
+		def name = parameter.name.text()
+		writer.println("| <<goals-$goal-parameters-details-$name,$name>>")
+		def type = parameter.type.text()
+		if (type.lastIndexOf('.') >= 0) {
+			type = type.substring(type.lastIndexOf('.') + 1)
+		}
+		writer.println("| `$type`")
+		def defaultValue = "${configuration[name].@'default-value'}"
+		if (defaultValue) {
+			writer.println("| `$defaultValue`")
+		}
+		else {
+			writer.println("|")
+		}
+		writer.println()
+	}
+	writer.println '|==='
+}
+
+private writeParameterDetails(PrintWriter writer, def parameters, def configuration, def sectionId) {
 	parameters.each { parameter ->
 		def name = parameter.name.text()
 		writer.println "[[$sectionId-$name]]"
-		writer.println "==== ${name}"
+		writer.println "==== `$name`"
+		writer.println(format(parameter.description.text()))
+		writer.println()
 		writer.println '[cols="10h,90"]'
 		writer.println '|==='
 		writer.println()
+		writer.println '| Name'
+		writer.println "| `$name`"
 		writer.println '| Type'
 		def type = parameter.type.text()
 		if (type.lastIndexOf('.') >= 0) {
@@ -36,12 +65,11 @@ private writeParameters(PrintWriter writer, def parameters, def configuration, d
 		def userProperty = "${configuration[name].text().replace('${', '`').replace('}', '`')}"
 		writer.println '| User property'
 		userProperty ? writer.println("| ${userProperty}") : writer.println("|")
-		writer.println '| Description'
-		writer.println("| ${format(parameter.description.text())}")
 		writer.println '| Since'
 		def since = parameter.since.text()
 		since ? writer.println("| `${since}`") : writer.println("|")
-
+		writer.println '| Required'
+		writer.println "| ${parameter.required.text()}"
 		writer.println()
 		writer.println '|==='
 	}
@@ -49,24 +77,30 @@ private writeParameters(PrintWriter writer, def parameters, def configuration, d
 
 def plugin = new XmlSlurper().parse("${project.build.outputDirectory}/META-INF/maven/plugin.xml" as File)
 String goalPrefix = plugin.goalPrefix.text()
-File goalsAdoc = new File(project.build.directory, "generated-resources/goals.adoc")
-goalsAdoc.parentFile.mkdirs()
-goalsAdoc.withPrintWriter { writer ->
+File goalsDir = new File(project.build.directory, "generated-resources/goals/")
+goalsDir.mkdirs()
+
+new File(goalsDir, "overview.adoc").withPrintWriter { writer ->
 	writer.println '[cols="1,3"]'
 	writer.println '|==='
 	writer.println '| Goal | Description'
 	writer.println()
 	plugin.mojos.mojo.each { mojo ->
-		writer.println "| ${goalPrefix}:${mojo.goal.text()}"
+		def goal = mojo.goal.text()
+		writer.println "| <<goals-$goal,${goalPrefix}:${mojo.goal.text()}>>"
 		writer.println "| ${format(mojo.description.text())}"
 		writer.println()
 	}
 	writer.println '|==='
-	plugin.mojos.mojo.each { mojo ->
-		def sectionId = "goals-${mojo.goal.text()}"
+}
+
+plugin.mojos.mojo.each { mojo ->
+	String goal = mojo.goal.text()
+	new File(goalsDir, "${goal}.adoc").withPrintWriter { writer ->
+		def sectionId = "goals-$goal"
 		writer.println()
 		writer.println("[[$sectionId]]")
-		writer.println("== `${goalPrefix}:${mojo.goal.text()}`")
+		writer.println("== `$goalPrefix:$goal`")
 		writer.println("`${plugin.groupId.text()}:${plugin.artifactId.text()}:${plugin.version.text()}:${mojo.goal.text()}`")
 		writer.println()
 		writer.println(format(mojo.description.text()))
@@ -74,19 +108,22 @@ goalsAdoc.withPrintWriter { writer ->
 		def parameters = mojo.parameters.parameter.findAll { it.editable.text() == 'true' }
 		def requiredParameters =  parameters.findAll { it.required.text() == 'true' }
 		if (requiredParameters.size()) {
-			def parametersSectionId = "$sectionId-required-parameters"
-			writer.println("[[$parametersSectionId]]")
+			writer.println("[[$sectionId-parameters-required]]")
 			writer.println("=== Required parameters")
-			writeParameters(writer, requiredParameters, mojo.configuration, parametersSectionId)
+			writeParametersTable(writer, goal, requiredParameters, mojo.configuration)
 			writer.println()
 		}
 		def optionalParameters = parameters.findAll { it.required.text() == 'false' }
 		if (optionalParameters.size()) {
-			def parametersSectionId = "$sectionId-optional-parameters"
-			writer.println("[[$parametersSectionId]]")
+			writer.println("[[$sectionId-parameters-optional]]")
 			writer.println("=== Optional parameters")
-			writeParameters(writer, optionalParameters, mojo.configuration, parametersSectionId)
+			writeParametersTable(writer, goal, optionalParameters, mojo.configuration)
 			writer.println()
 		}
+		def detailsSectionId = "$sectionId-parameters-details"
+		writer.println("[[$detailsSectionId]]")
+		writer.println("=== Parameter details")
+		writeParameterDetails(writer, parameters, mojo.configuration, detailsSectionId)
+		writer.println()
 	}
 }
