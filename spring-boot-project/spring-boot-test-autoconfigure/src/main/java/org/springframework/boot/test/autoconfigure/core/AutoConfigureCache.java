f2df1fee884f9f148067a8bf98e896a1e2413ac6
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-03-30 09:43:37 +0200

    Create spring-boot-cache module

--- spring-boot-project/spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/core/AutoConfigureCache.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/main/java/org/springframework/boot/test/autoconfigure/core/AutoConfigureCache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.cache.CacheType;
+import org.springframework.boot.cache.autoconfigure.CacheType;
 import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.support.NoOpCacheManager;
