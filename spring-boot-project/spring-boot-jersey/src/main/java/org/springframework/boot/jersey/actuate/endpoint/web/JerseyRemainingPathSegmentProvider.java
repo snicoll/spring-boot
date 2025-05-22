ac94b25718c7279a727f41215bdb996dbb15f1e7
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-05-22 10:20:34 +0200

    Move Actuator Jersey infrastructure to spring-boot-jersey

--- spring-boot-project/spring-boot-jersey/src/main/java/org/springframework/boot/jersey/actuate/endpoint/web/JerseyRemainingPathSegmentProvider.java
+++ spring-boot-project/spring-boot-jersey/src/main/java/org/springframework/boot/jersey/actuate/endpoint/web/JerseyRemainingPathSegmentProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,18 @@
  * limitations under the License.
  */
 
+package org.springframework.boot.jersey.actuate.endpoint.web;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+
 /**
- * Jersey support for actuator endpoints.
+ * Strategy interface used to provide the remaining path segments for a Jersey actuator
+ * endpoint.
+ *
+ * @author Madhura Bhave
  */
-package org.springframework.boot.actuate.endpoint.web.jersey;
+interface JerseyRemainingPathSegmentProvider {
+
+	String get(ContainerRequestContext requestContext, String matchAllRemainingPathSegmentsVariable);
+
+}
