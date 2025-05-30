dc7c7363b308701d59dc447e53491fac9de2906c
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-05-30 15:05:36 +0200

    Polish

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/exchanges/servlet/HttpExchangesFilter.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/exchanges/servlet/HttpExchangesFilter.java
@@ -31,7 +31,6 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.boot.actuate.web.exchanges.HttpExchange;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.Include;
-import org.springframework.boot.actuate.web.exchanges.reactive.HttpExchangesWebFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -57,7 +56,7 @@ public class HttpExchangesFilter extends OncePerRequestFilter implements Ordered
 	private final Set<Include> includes;
 
 	/**
-	 * Create a new {@link HttpExchangesWebFilter} instance.
+	 * Create a new instance.
 	 * @param repository the repository used to record events
 	 * @param includes the include options
 	 */
