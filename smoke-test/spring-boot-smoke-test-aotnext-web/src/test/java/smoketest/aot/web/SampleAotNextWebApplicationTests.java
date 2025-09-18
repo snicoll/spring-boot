3081b0526a5bb65ed386ded3ff01d3a2edc3647c
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-09-18 19:47:49 +0200

    Add sample that runs refreshForAotProcessing

---
+++ smoke-test/spring-boot-smoke-test-aotnext-web/src/test/java/smoketest/aot/web/SampleAotNextWebApplicationTests.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smoketest.aot.web;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+class SampleAotNextWebApplicationTests {
+
+	@Test
+	void refreshForAotProcessing() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.setBeanNameGenerator(new AotBeanNameGenerator());
+		context.setAllowBeanDefinitionOverriding(true);
+		context.register(SampleAotNextWebApplication.class);
+		context.getEnvironment()
+			.getPropertySources()
+			.addFirst(new MapPropertySource("AOT", Map.of("spring.aot.condition-evaluation", "true")));
+		context.refreshForAotProcessing(new RuntimeHints());
+		for (String beanDefinitionName : context.getBeanDefinitionNames()) {
+			System.out.println(">> name " + beanDefinitionName);
+		}
+	}
+
+}
