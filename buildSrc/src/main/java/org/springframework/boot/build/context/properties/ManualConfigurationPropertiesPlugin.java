69d8f5e7e36733084b772c960b13c6727b689ef3
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-11-12 11:03:38 +0100

    Add plugin

---
+++ buildSrc/src/main/java/org/springframework/boot/build/context/properties/ManualConfigurationPropertiesPlugin.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.build.context.properties;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.language.base.plugins.LifecycleBasePlugin;
+
+/**
+ * {@link Plugin} for projects that <em>only</em >define manual metadata. When applied,
+ * the plugin registers a {@link CheckManualSpringConfigurationMetadata} task and
+ * configures the {@code check} task to depend upon it.
+ *
+ * @author Andy Wilkinson
+ * @author Stephane Nicoll
+ */
+public class ManualConfigurationPropertiesPlugin implements Plugin<Project> {
+
+	/**
+	 * Name of the {@link CheckAdditionalSpringConfigurationMetadata} task.
+	 */
+	public static final String CHECK_MANUAL_SPRING_CONFIGURATION_METADATA_TASK_NAME = "checkManualSpringConfigurationMetadata";
+
+	@Override
+	public void apply(Project project) {
+		project.getPlugins().withType(JavaPlugin.class, (javaPlugin) -> registerCheckAdditionalMetadataTask(project));
+	}
+
+	private void registerCheckAdditionalMetadataTask(Project project) {
+		TaskProvider<CheckManualSpringConfigurationMetadata> checkConfigurationMetadata = project.getTasks()
+			.register(CHECK_MANUAL_SPRING_CONFIGURATION_METADATA_TASK_NAME,
+					CheckManualSpringConfigurationMetadata.class);
+		checkConfigurationMetadata.configure((check) -> {
+			SourceSet mainSourceSet = project.getExtensions()
+				.getByType(JavaPluginExtension.class)
+				.getSourceSets()
+				.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+			check.setSource(mainSourceSet.getResources());
+			check.include("META-INF/spring-configuration-metadata.json");
+			check.getReportLocation()
+				.set(project.getLayout()
+					.getBuildDirectory()
+					.file("reports/manual-spring-configuration-metadata/check.txt"));
+		});
+		project.getTasks()
+			.named(LifecycleBasePlugin.CHECK_TASK_NAME)
+			.configure((check) -> check.dependsOn(checkConfigurationMetadata));
+	}
+
+}
